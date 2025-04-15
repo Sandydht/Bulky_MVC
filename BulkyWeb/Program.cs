cf221a12920a63669f50aab84c4b72e0f73e8b4f
sandy <sandydwi75@gmail.com>
2025-04-15 23:17:30 +0700

    feat: update crud

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,11 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
 var app = builder.Build();
 
