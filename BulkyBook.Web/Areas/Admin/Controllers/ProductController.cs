0c35b783da0659ffaefec216f117d8a721764b6a
sandy <sandydwi75@gmail.com>
2025-04-27 18:45:55 +0700

    feat: update file upload feature

--- BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
@@ -56,7 +56,18 @@ public class ProductController : Controller
             if (file != null)
             {
                 string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                string productPath = Path.Combine(wwwRootPath, @"images\product");
+                string productPath = Path.Combine(wwwRootPath, @"images/product");
+
+                if (!string.IsNullOrEmpty(productVm.Product.ImageUrl))
+                {
+                    // Delete the old image
+                    var oldImagePath = Path.Combine(wwwRootPath, productVm.Product.ImageUrl.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
 
                 using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                 {
@@ -65,8 +76,16 @@ public class ProductController : Controller
 
                 productVm.Product.ImageUrl = @"\images\product\" + fileName;
             }
+
+            if (productVm.Product.Id == 0)
+            {
+                _unitOfWork.Product.Add(productVm.Product);
+            }
+            else
+            {
+                _unitOfWork.Product.Update(productVm.Product);
+            }
             
-            _unitOfWork.Product.Add(productVm.Product);
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully";
             return RedirectToAction("Index");
